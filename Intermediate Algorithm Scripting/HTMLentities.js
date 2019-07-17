5c300088fc33929c85644ae8e9fa3d7ba3b96438
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2019-07-17 22:50:44 +0530

    Added HTML entities algorithm script

--- Intermediate Algorithm Scripting/HTMLentities.js
+++ Intermediate Algorithm Scripting/HTMLentities.js
@@ -1,6 +1,27 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
-function convertHTML(str) {}
+function convertHTML(str) {
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case '&':
+        str = str.replace(str[i], '&amp;');
+        break;
+      case '<':
+        str = str.replace(str[i], '&lt;');
+        break;
+      case '>':
+        str = str.replace(str[i], '&gt;');
+        break;
+      case '"':
+        str = str.replace(str[i], '&quot;');
+        break;
+      case "'":
+        str = str.replace(str[i], '&​apos;'); // there is a hidden character 'dot' here in '&apos;', so remove it by going to 'https://stephengrider.github.io/JSPlaygrounds/' and then paste it in FCC editor
+        break;
+    }
+  }
+  return str;
+}
 
 console.log(convertHTML('Dolce & Gabbana'));
 console.log(convertHTML('Hamburgers < Pizza < Tacos'));
