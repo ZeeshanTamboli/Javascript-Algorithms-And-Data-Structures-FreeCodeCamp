e47bf4adf2502ed3d53780d05323ec88ce3a88e3
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2019-07-22 22:28:26 +0530

    binary agents algorithm

---
+++ Intermediate Algorithm Scripting/binaryAgents.js
@@ -0,0 +1,22 @@
+// Return an English translated sentence of the passed binary string.
+
+// The binary string will be space separated.
+
+const binaryAgent = str => {
+  return str
+    .split(' ')
+    .map(binaryString => String.fromCharCode(parseInt(binaryString, 2))) // parseInt pass the binary code string whose base is 2 i.e binary so it will convert to a number. From that number get the specific letter using charCodeAt
+    .join('');
+};
+
+console.log(
+  binaryAgent(
+    '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111'
+  )
+);
+
+console.log(
+  binaryAgent(
+    '01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001'
+  )
+);
