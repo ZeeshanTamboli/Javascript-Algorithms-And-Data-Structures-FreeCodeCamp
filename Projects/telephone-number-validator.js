6e9c5915da1ac275f8ca046ab83de5a8d4b7dd07
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2019-08-10 21:46:26 +0530

    Telephone number validator project

--- Projects/telephone-number-validator.js
+++ Projects/telephone-number-validator.js
@@ -11,7 +11,8 @@
 // For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
 
 function telephoneCheck(str) {
-  const regex = /^1\s/;
+  //  '1 ' '1' '(555)'  '555' ' ' '-' '555' ' ' '-' '5555'
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[ .-]?\d{3}[ .-]?\d{4}$/;
   return regex.test(str);
 }
 
